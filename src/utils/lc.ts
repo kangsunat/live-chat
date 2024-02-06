aa0c24c032c753e0155762d3d4984f6bc7dd3596
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-02-06 12:04:24 +0700

    env

--- src/utils/lc.ts
+++ src/utils/lc.ts
@@ -7,8 +7,8 @@ const customerAPI = new Customer.RTM(WebSocket, "organization id");
 async function LcConnection() {
   await Promise.all([agentAPI.connect(), customerAPI.connect()]);
   await Promise.all([
-    agentAPI.login("Bearer dal:token"),
-    customerAPI.login("Bearer dal:token"),
+    agentAPI.login(`Bearer dal:${process.env.LC_TOKEN}`),
+    customerAPI.login(`Bearer dal:${process.env.LC_TOKEN}`),
   ]);
 
   const { chat_id } = await customerAPI.startChat();
